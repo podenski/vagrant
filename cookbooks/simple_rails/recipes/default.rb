9af4f3fdb14cbfd663430186b2617c3786275861
Patrick Podenski <podenski@me.com>
2012-12-04 20:15:27 -0800

    Added iptables stop temporarily

--- cookbooks/simple_rails/recipes/default.rb
+++ cookbooks/simple_rails/recipes/default.rb
@@ -1,3 +1,11 @@
 package "svn" do
 	action :install
 end
+
+# temporarily disable firewall so that rails can poke through to my
+# host workstation web browser
+# Need to change this to a firewall rule (look it up from prior notes)
+service "iptables" do
+	action :stop
+end
+
