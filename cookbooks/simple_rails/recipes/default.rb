d26e040f66d5adc57fb5f7d9fb2c6e05246b8a7a
unknown <chq-patrickp@CHQ-PATRICKP-L.expd>
2012-12-05 07:51:53 -0800

    added git install

--- cookbooks/simple_rails/recipes/default.rb
+++ cookbooks/simple_rails/recipes/default.rb
@@ -2,6 +2,10 @@ package "svn" do
 	action :install
 end
 
+package "git" do
+	action :install
+end
+
 # temporarily disable firewall so that rails can poke through to my
 # host workstation web browser
 # Need to change this to a firewall rule (look it up from prior notes)
